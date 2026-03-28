b8d13fdec36908de5b1bf18e780847cd56708502
AndyEmerik1045 <h1101241045@student.untan.ac.id>
2026-03-28 09:14:16 +0700

    perbaiki login

--- apps/backend/src/auth.ts
+++ apps/backend/src/auth.ts
@@ -4,7 +4,7 @@ export function createOAuthClient() {
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI
+    process.env.GOOGLE_REDIRECT_URI // connect ke google cloud console dengan link dari .env
   );
 }
 
